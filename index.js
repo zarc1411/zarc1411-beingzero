2ccf1ccba290cde0824da75d2518b5ebfdd0a056
zarc1411 <zarc1483@gmail.com>
2019-12-19 16:00:54 +0530

    Defined JSON objects

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT||3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
-
+var obj={}
+obj.name = "Being Zero";
+obj.college = "CMRCET";
+obj.rno = "17H51A05E0";
+app.get('/data', (req, res) => res.json(obj))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
